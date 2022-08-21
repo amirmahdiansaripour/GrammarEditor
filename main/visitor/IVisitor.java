dcaea8ed5a9a8b1bf545dd109b72263cef03d481
Amirmahdi Ansaripour <92050925+amirmahdiansaripour@users.noreply.github.com>
2022-08-21 12:12:01 -0700

    Delete main directory

--- main/visitor/IVisitor.java
+++
@@ -1,7 +0,0 @@
-package main.visitor;
-import main.ast.*;
-
-public interface IVisitor<T> {
-    T visit(Text text);
-    T visit(Sentence sentence);
-}
--- main/visitor/Visitor.java
+++
@@ -1,8 +0,0 @@
-package main.visitor;
-import main.ast.*;
-public class Visitor<T> implements IVisitor<T> {
-    @Override
-    public T visit(Text text){return null;}
-    @Override
-    public T visit(Sentence sentence){return null;}
-}
