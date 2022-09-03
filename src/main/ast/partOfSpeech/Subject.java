89441e977c52c4ac45f127574c08e0cebfd7b859
amirmahdi <ansaripouramirmahdi@gmail.com>
2022-09-03 13:35:58 -0700

    count correspondence nearly completed

--- src/main/ast/partOfSpeech/Subject.java
+++ src/main/ast/partOfSpeech/Subject.java
@@ -14,67 +14,96 @@ public class Subject extends Word {
             "people", "the blind", "the poor", "the rich"));
     protected static ArrayList<String> singularExceptions = new ArrayList<String>(Arrays.asList("mumps",
             "measles", "physics", "mathematics", "news", "information", "evidence", "equipment"));
-    private ArrayList<String> count;
+//    private ArrayList<String> count;
+    private String count;
     public Subject(String t, Boolean cap, int line_){
         super(t, cap, line_);
-        count = new ArrayList<String>();
-        setCount();
-//        System.out.println(text + " " + count);
-//        nounDataset = makeDataSet("", nounDataset);
+//        count = new ArrayList<String>();
+        setCount(text);
+        System.out.println(text + ";;count;;" + count);
     }
 
-    public Boolean checkSuffix(String sub, String suffix){
-        if(sub.length() > suffix.length() && sub.substring(sub.length() - suffix.length()).equals(suffix)){
-            String withoutSuffix = sub.substring(0, sub.length() - suffix.length());
-            if(singularExceptions.contains(withoutSuffix) || pluralExceptions.contains(withoutSuffix)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void setCountBasedOnPronouns(String sub, int firstIndex){
+    public void setCountBasedOnPronouns(String sub){
         if(sub.equals("he") || sub.equals("she") || sub.equals("it") || singularExceptions.contains(sub)){
-            count.add("has"); count.add("was"); count.add("is"); count.add("does");
+            count = "third singular";
+//            count.add("has"); count.add("was"); count.add("is"); count.add("does");
         }
         else if(sub.equals("i")){
-            count.add("have"); count.add("was"); count.add("am"); count.add("do");
+            count = "first singular";
+//            count.add("have"); count.add("was"); count.add("am"); count.add("do");
         }
         else if(sub.equals("you") || sub.equals("we") || sub.equals("they") || pluralExceptions.contains(sub)){
-            count.add("have"); count.add("were"); count.add("are"); count.add("do");
-        }
-        else if(sub.contains("a ") || sub.contains("an ")){
-            count.add("has"); count.add("was"); count.add("is"); count.add("does");
-        }
-        else if(firstIndex != -1){
-            String first = sub.substring(0, firstIndex);
-
+            count = "plural";
+//            count.add("have"); count.add("were"); count.add("are"); count.add("do");
         }
     }
 
-    public void setCount(){
-        String sub = text.toLowerCase();
+    public void checkSuffix(String sub, String suffix){
+        int lastIndex = sub.lastIndexOf(' ');
+        String lastPart = sub;
+        if(lastIndex != -1) lastPart = sub.substring(lastIndex + 1);
+
+        if (lastPart.length() - suffix.length() > 0 &&
+                lastPart.substring(lastPart.length() - suffix.length()).equals(suffix)) {
+            String withoutSuffix = lastPart.substring(0, lastPart.length() - suffix.length());
+            if (singularExceptions.contains(withoutSuffix) || pluralExceptions.contains(withoutSuffix)){
+                errors.add(new GrammarError.IsntCorrect(line, text));
+                count = "wrong";    // words such as informations, peoples, are wrong
+            }
+            else {
+                count = "plural";
+            }
+        }
+    }
+
+    public void setCountBasedOnPrefix(String sub){
+        checkSuffix(sub, "s");
+        checkSuffix(sub, "es");
+    }
+
+    public void setCountBasedOnPrev(String sub){
         int firstSpaceIndex = text.indexOf(' ');
+        int secondSpaceIndex = text.indexOf(' ', firstSpaceIndex + 1);
         int lastSpaceIndex = text.lastIndexOf(' ');
-        setCountBasedOnPronouns(sub, firstSpaceIndex);
+        if(firstSpaceIndex != -1) {
+            String firstPart = sub.substring(0, firstSpaceIndex);
+            if(firstPart.equals("most") || firstPart.equals("more") || firstPart.equals("some") ||
+                    firstPart.equals("the") || (secondSpaceIndex != -1 && (sub.substring(0, secondSpaceIndex).equals("some of")))){
+                setCount(sub.substring(lastSpaceIndex + 1));
+            }
+            else if(firstPart.equals("this") || firstPart.equals("that")){
+                setCount(sub.substring(lastSpaceIndex + 1));
+                if(count.equals("plural"))
+                    errors.add(new GrammarError.BeSingular(line, sub.substring(lastSpaceIndex + 1)));
+            }
+            else if(firstPart.equals("these") || firstPart.equals("those")){
+                setCount(sub.substring(lastSpaceIndex + 1));
+                if(!count.equals("wrong") && !count.equals("plural"))
+                    errors.add(new GrammarError.BePlural(line, sub.substring(lastSpaceIndex + 1)));
+            }
+            else if(firstPart.equals("few") || (secondSpaceIndex != -1 && (sub.substring(0, secondSpaceIndex).equals("a few")
+            || sub.substring(0, secondSpaceIndex).equals("a little")))){
+                setCount(sub.substring(lastSpaceIndex + 1));
+                if(!count.equals("plural") && !count.equals("wrong")){
+                    errors.add(new GrammarError.BePlural(line, sub.substring(lastSpaceIndex + 1)));
+                }
+            }
+            if (firstPart.equals("a") || firstPart.equals("an")) {
+                count = "third singular";
+            }
+        }
 
-//        else{ // s and es for plurals
-//            Boolean sSuffix = checkSuffix(sub, "s");
-//            Boolean esSuffix = checkSuffix(sub, "es");
-//            if(!(sSuffix && esSuffix)){
-//                errors.add(new GrammarError.IsntCorrect(line, text));
-//            }
-//            else{
-//                if(Character.isUpperCase(text.charAt(0))){   //Proper nouns that end in s or es
-//                    count.add("has"); count.add("was"); count.add("is"); count.add("does");
-//                }
-//                else{
-//                   count.add("have"); count.add("were"); count.add("are"); count.add("do");
-//                }
-//            }
-//        }
+    }
 
-
+    public void setCount(String t){
+        String sub = t.toLowerCase();
+        setCountBasedOnPronouns(sub);
+        if(count == null){
+            setCountBasedOnPrev(sub);
+        }
+        if(count == null){
+            setCountBasedOnPrefix(sub);
+        }
     }
     @Override
     public <T> T accept(IVisitor<T> visitor) {
